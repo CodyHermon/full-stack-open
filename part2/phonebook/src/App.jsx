6eae421ab2c37f330ac9507924e83d648c5c05b1
Cody Hermon <codyhermon@gmail.com>
2023-10-30 16:28:46 -0700

    created phonebook exercise

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,66 @@
+import { useState } from 'react';
+
+import Persons from './components/Persons';
+import PersonForm from './components/PersonForm';
+import Filter from './components/Filter';
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
+  ]);
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+  const [filter, setFilter] = useState('');
+
+  const handleFilterChange = (e) => {
+    setFilter(e.target.value);
+  };
+
+  const handleNameChange = (e) => {
+    setNewName(e.target.value);
+  };
+
+  const handleNumberChange = (e) => {
+    setNewNumber(e.target.value);
+  };
+
+  const addPerson = (e) => {
+    e.preventDefault();
+    if (persons.some((e) => e.name === newName)) {
+      alert(`${newName} is already added to phonebook`);
+      return;
+    }
+    const personObject = {
+      name: newName,
+      number: newNumber,
+    };
+
+    setPersons(persons.concat(personObject));
+  };
+
+  const phoneList = persons.filter((person) =>
+    person.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter filter={filter} onChangeFilter={handleFilterChange} />
+      <h3>add a new</h3>
+      <PersonForm
+        newName={newName}
+        newNumber={newNumber}
+        handleNameChange={handleNameChange}
+        handleNumberChange={handleNumberChange}
+        addPerson={addPerson}
+      />
+      <h2>Numbers</h2>
+      <Persons persons={phoneList} />
+    </div>
+  );
+};
+
+export default App;
---
+++ part2/phonebook/src/components/Filter/index.jsx
@@ -0,0 +1,9 @@
+const Filter = ({ filter, onChangeFilter }) => {
+  return (
+    <div>
+      filter shown with <input value={filter} onChange={onChangeFilter} />
+    </div>
+  );
+};
+
+export default Filter;
