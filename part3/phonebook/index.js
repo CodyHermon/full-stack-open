8aabb145bb953909967cf32f53288c1b9c4cf75c
Cody Hermon <codyhermon@gmail.com>
2023-11-10 22:40:55 -0800

    added error handling

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -20,6 +20,20 @@ app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :body')
 );
 
+const errorHandler = (error, request, response, next) => {
+  console.log(error(error.message));
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  }
+
+  next(error);
+};
+
+const unknownEndpoint = (request, resposne) => {
+  response.status(404).send({ error: 'unknown endpoint' });
+};
+
 app.get('/api/persons', (request, response) => {
   Person.find({}).then((persons) => {
     response.json(persons);
@@ -35,16 +49,23 @@ app.get('/info', (request, response) => {
 });
 
 app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then((person) => {
-    response.json(person);
-  });
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== id);
-
-  response.status(204).end();
+  Person.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
 app.post('/api/persons', (request, response) => {
@@ -66,8 +87,25 @@ app.post('/api/persons', (request, response) => {
   });
 });
 
-const PORT = process.env.PORT;
+app.put('/api/persons/:id', (request, response) => {
+  const body = request.body;
 
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      response.json(updatedPerson);
+    })
+    .catch((error) => next(error));
+});
+
+app.use(unknownEndpoint);
+app.use(errorHandler);
+
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
