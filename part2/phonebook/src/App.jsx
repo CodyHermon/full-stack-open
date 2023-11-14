dae2a9cc901ad0bea5de33c4a62a09153e0f6e16
Cody Hermon <codyhermon@gmail.com>
2023-11-13 16:21:17 -0800

    added eslint

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -46,7 +46,9 @@ const App = () => {
       number: newNumber,
     };
 
-    const existingPerson = persons.find((person) => person.name === newName);
+    const existingPerson = persons.find(
+      (person) => person.name.toLowerCase() === newName.toLowerCase()
+    );
 
     if (existingPerson) {
       if (
@@ -57,6 +59,15 @@ const App = () => {
         personsService
           .update(existingPerson.id, personObject)
           .then((returnedPerson) => {
+            if (!returnedPerson) {
+              setErrorMessage(
+                `Information of '${existingPerson.name}' has already been removed from server`
+              );
+              setTimeout(() => {
+                setErrorMessage(null);
+              }, 5000);
+              return;
+            }
             setPersons(
               persons.map((person) =>
                 person.id !== existingPerson.id ? person : returnedPerson
@@ -65,30 +76,39 @@ const App = () => {
             setNotificationMessage(
               `Changed ${newName}'s number to ${newNumber}`
             );
+            setNewName('');
+            setNewNumber('');
             setTimeout(() => {
               setNotificationMessage(null);
             }, 5000);
           })
           .catch((e) => {
-            console.log(e);
-            setErrorMessage(
-              `Information of '${existingPerson.name}' has already been removed from server`
-            );
-            setTimeout(() => {
-              setErrorMessage(null);
-            }, 5000);
+            if (e.response.status === 400) {
+              setErrorMessage(e.response.data.error);
+              setTimeout(() => {
+                setErrorMessage(null);
+              }, 5000);
+            }
           });
 
         return;
       }
     } else {
-      personsService.create(personObject).then((returnedPerson) => {
-        setNotificationMessage(`Added ${newName}`);
-        setTimeout(() => {
-          setNotificationMessage(null);
-        }, 5000);
-        setPersons(persons.concat(returnedPerson));
-      });
+      personsService
+        .create(personObject)
+        .then((returnedPerson) => {
+          setNotificationMessage(`Added ${newName}`);
+          setTimeout(() => {
+            setNotificationMessage(null);
+          }, 5000);
+          setPersons(persons.concat(returnedPerson));
+        })
+        .catch((e) => {
+          setErrorMessage(e.response.data.error);
+          setTimeout(() => {
+            setErrorMessage(null);
+          }, 5000);
+        });
     }
   };
 
@@ -113,7 +133,7 @@ const App = () => {
   };
 
   const phoneList = persons.filter((person) =>
-    person.name.toLowerCase().includes(filter.toLowerCase())
+    person?.name.toLowerCase().includes(filter.toLowerCase())
   );
 
   return (
--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -21,16 +21,18 @@ app.use(
 );
 
 const errorHandler = (error, request, response, next) => {
-  console.log(error(error.message));
+  console.log(error.message);
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' });
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message });
   }
 
   next(error);
 };
 
-const unknownEndpoint = (request, resposne) => {
+const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' });
 };
 
@@ -48,7 +50,7 @@ app.get('/info', (request, response) => {
   });
 });
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
     .then((person) => {
       if (person) {
@@ -60,15 +62,15 @@ app.get('/api/persons/:id', (request, response) => {
     .catch((error) => next(error));
 });
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
-    .then((result) => {
+    .then(() => {
       response.status(204).end();
     })
     .catch((error) => next(error));
 });
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body;
 
   if (!body.name || !body.number) {
@@ -82,20 +84,22 @@ app.post('/api/persons', (request, response) => {
     number: body.number,
   });
 
-  person.save().then((savedPerson) => {
-    response.json(savedPerson);
-  });
+  person
+    .save()
+    .then((savedPerson) => {
+      response.json(savedPerson);
+    })
+    .catch((error) => next(error));
 });
 
-app.put('/api/persons/:id', (request, response) => {
-  const body = request.body;
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body;
 
-  const person = {
-    name: body.name,
-    number: body.number,
-  };
-
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  Person.findByIdAndUpdate(
+    request.params.id,
+    { name, number },
+    { new: true, runValidators: true, context: 'query' }
+  )
     .then((updatedPerson) => {
       response.json(updatedPerson);
     })
